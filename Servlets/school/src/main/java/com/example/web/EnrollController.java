d53c3975af59dde2b9ded41680e0e1c0f0e7342c
RajithaOpanayaka <rajithaopanyaka.ro@gmail.com>
2022-05-18 19:22:04 +0530

    add enroll controller

--- Servlets/school/src/main/java/com/example/web/EnrollController.java
+++ Servlets/school/src/main/java/com/example/web/EnrollController.java
@@ -3,9 +3,13 @@ package com.example.web;
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.*;
+import java.util.ArrayList;
+
 import com.example.model.subject.Subject;
+import com.example.model.student.Student;
 import com.example.db.LocalDB;
-public class SubjectController extends HttpServlet{
+
+public class EnrollController extends HttpServlet{
 
     private LocalDB db;
 
@@ -25,16 +29,27 @@ public class SubjectController extends HttpServlet{
             return;
         }
         out.println("Subject: "+subject.getName());
+        ArrayList<String> students = subject.getStudents();
+        for(int i=0;i< students.size();i++){
+            out.println("Student id: "+students.get(i));
+        }
     }
     public void doPost(HttpServletRequest request,HttpServletResponse response) throws IOException,ServletException{
-        String id = request.getParameter("id");
-        String name = request.getParameter("name");
+        String subjectId = request.getParameter("subject");
+        String studentId = request.getParameter("student");
 
-        Subject subject = new Subject(name,id);
-        db.addSubject(subject, id);
+        Subject subject = db.getSubject(subjectId);
+        Student student  = db.getStudent(studentId);
 
         PrintWriter out = response.getWriter();
         response.setContentType("text/html");
-        out.println("Successfully added");
+
+        if(subject == null || student == null){
+            out.println("Invalid inputs.");
+            return;
+        }
+        student.addSubject(subjectId);
+        subject.addStudent(studentId);
+        out.println("Successfully enrolled");
     }
 }
---
+++ Servlets/school/src/main/webapp/addSubject.jsp
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>School</title>
+        <h3>Add Subject</h3>
+        <form method="POST" action="subject.do">
+            <input type="text" name="id" placeholder="id">
+            <input type="text" name="name" placeholder="name">
+            <button>Submit</button>
+        </form>
+    </head>
+</html>
